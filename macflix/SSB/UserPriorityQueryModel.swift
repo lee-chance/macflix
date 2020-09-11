3c6dc4337f7e6d8fbe516ff6004a3e5601c7f03d
Changsu Lee <cslee@Changsuui-MacBookPro.local>
2020-09-12 00:11:13 +0900

    cs 0912 00:11

--- macflix/SSB/UserPriorityQueryModel.swift
+++ macflix/SSB/UserPriorityQueryModel.swift
@@ -15,11 +15,12 @@ protocol PriorityModelProtocol: class {
 class UserPriorityQueryModel: NSObject{
     
     var delegate: PriorityModelProtocol!
-    let urlPath = URL_PATH + "IOS/getPriorityList.jsp?seq=\(LOGGED_IN_SEQ)"
+    let urlPath = URL_PATH + "CSJSP/getPriorityList.jsp?seq=\(LOGGED_IN_SEQ)"
     
     func getPriorityList() {
         
         let url: URL = URL(string: urlPath)!
+        print(urlPath)
         let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
         
         let task = defaultSession.dataTask(with: url) {(data, respone, error) in
@@ -32,23 +33,23 @@ class UserPriorityQueryModel: NSObject{
     }
     
     func parseJSON2(_ data: Data) {
-            var jsonResult = String(data: data, encoding: .utf8)!
-    //        do {
-    //            jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! String
-    //        } catch let error as NSError {
-    //            print(error)
-    //        }
-            jsonResult = jsonResult.replacingOccurrences(of: "\r\n", with: "")
-            let splitedList = jsonResult.components(separatedBy: ", ")
-            var resultList: [String] = []
-            for i in 0..<splitedList.count {
-                resultList.append(splitedList[i])
-            }
+        var jsonResult = String(data: data, encoding: .utf8)!
+        //        do {
+        //            jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! String
+        //        } catch let error as NSError {
+        //            print(error)
+        //        }
+        jsonResult = jsonResult.replacingOccurrences(of: "\r\n", with: "")
+        let splitedList = jsonResult.components(separatedBy: ", ")
+        var resultList: [String] = []
+        for i in 0..<splitedList.count {
+            resultList.append(splitedList[i])
+        }
         
         DispatchQueue.main.async(execute: {() -> Void in
             self.delegate.itemDownloaded(items: resultList)
         })
-        }
+    }
     
-        
+    
 }
